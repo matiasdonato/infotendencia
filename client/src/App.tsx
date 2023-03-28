c004c89d94b5db1fb8c3647fd1242accd0f7a0cb
Matias <matiasdonato14@gmail.com>
2023-03-28 18:50:08 -0300

    adsense update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,36 +3,50 @@ import Nav from "./components/Nav";
 import Articles from "./components/Articles";
 import CreateArticles from "./components/CreateArticles";
 import HomePage from "./components/HomePage";
+import { Helmet } from "react-helmet";
 
 function App() {
 
   
   
   // <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2912035208235100" crossorigin="anonymous"></script>
-
-  const myStyles = {
-    display: 'block',
-  };
   
+  //@ts-nocheck
+  //@ts-ignore
+
+
+
+
+  // ins.className = "adsbygoogle"
+  // //@ts-ignore
+  // ins.style = "display:block"
+  // //@ts-ignore
+  // ins.data-ad-client = "ca-pub-2912035208235100" 
+  
+
+
   return (
     <div className="app">
       <Nav/>
       <Route exact path={"/"} render={() => <HomePage/>} />
       <Route path={"/articles"} render={() => <Articles/>} />
       <Route path={"/createarticles"} render={() => <CreateArticles/>} />
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2912035208235100"
-      crossOrigin="anonymous"></script>
-      <ins className="adsbygoogle"
-      style={myStyles}
-      data-ad-client="ca-pub-2912035208235100"
-      data-ad-slot="6622030279"
-      data-ad-format="auto"
-      data-full-width-responsive="true"></ins>
-      <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-      </script>
-
-      
+      <div>
+        <Helmet>
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2912035208235100" crossOrigin="anonymous"></script>
+          <script>
+            {`(adsbygoogle = window.adsbygoogle || []).push({});`}
+          </script>
+        </Helmet>
+        <ins
+          className="adsbygoogle"
+          style={{ display: 'block' }}
+          data-ad-client="ca-pub-2912035208235100"
+          data-ad-slot="6622030279"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
+      </div>
     </div>
   );
 }
